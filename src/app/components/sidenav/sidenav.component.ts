cca980eba521a661266278480095fcd8c0c54866
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-04-04 22:27:06 -0400

    sidenav working with some issues

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -7,48 +7,80 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-    items!: MenuItem[];
+  items!: MenuItem[];
 
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'File',
-                icon: 'pi pi-fw pi-file',
-                items: [
-                    {
-                        label: 'New',
-                        icon: 'pi pi-fw pi-plus',
-                        items: [
-                            {
-                                label: 'Bookmark',
-                                icon: 'pi pi-fw pi-bookmark'
-                            },
-                            {
-                                label: 'Video',
-                                icon: 'pi pi-fw pi-video'
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Delete',
-                        icon: 'pi pi-fw pi-trash'
-                    },
-                    {
-                        separator: true
-                    },
-                    {
-                        label: 'Export',
-                        icon: 'pi pi-fw pi-external-link'
-                    }
-                ]
-            },
-            {
-                separator: true
-            },
-            {
-                label: 'Quit',
-                icon: 'pi pi-fw pi-power-off'
-            }
-        ];
-    }
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-fw pi-file',
+      },
+      {
+        label: 'Bens Móveis',
+        icon: 'pi pi-fw pi-file',
+        items: [
+          {
+            label: 'Ingresso de Bens Móveis',
+            icon: 'pi pi-fw pi-plus',
+          },
+          {
+            label: 'Incorporação de Bens Móveis',
+            icon: 'pi pi-fw pi-trash',
+          },
+          {
+            label: 'Inventário',
+            icon: 'pi pi-fw pi-plus',
+          },
+          {
+            label: 'Desfazimento/Baixa',
+            icon: 'pi pi-fw pi-plus',
+          },
+          {
+            label: 'Ingresso e saída Temporária',
+            icon: 'pi pi-fw pi-plus',
+          },
+          {
+            label: 'Procedimentos contábeis',
+            icon: 'pi pi-fw pi-plus',
+            items: [
+              {
+                label: 'Avaliação',
+                icon: 'pi pi-fw pi-plus',
+              },
+              {
+                label: 'Reavaliação',
+                icon: 'pi pi-fw pi-plus',
+              },
+              {
+                label: 'Redução ao valor recuperável',
+                icon: 'pi pi-fw pi-plus',
+              },
+              {
+                label: 'Depreciação',
+                icon: 'pi pi-fw pi-plus',
+              },
+              {
+                label: 'Amortização',
+                icon: 'pi pi-fw pi-plus',
+              },
+              {
+                label: 'Exaustão',
+                icon: 'pi pi-fw pi-plus',
+              },
+            ],
+          },
+          {
+            separator: true,
+          },
+        ],
+      },
+      {
+        separator: true,
+      },
+      {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off',
+      },
+    ];
+  }
 }
