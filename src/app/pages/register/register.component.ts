b091a1f709baa4645f04bb81d312a1430c18906e
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-04-27 19:07:51 -0400

    register method created and working at register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+interface RegisterResponse {
+  acess_token: string;
+}
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-name = '';
-email = '';
-password = '';
+  name = '';
+  email = '';
+  password = '';
+  showError = false;
+  constructor(private http: HttpClient, private router: Router) {}
 
+  register() {
+    const credentials = {
+      username: this.name,
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http
+      .post<RegisterResponse>(
+        'http://localhost:3000/auth/register',
+        credentials
+      )
+      .subscribe(
+        (response) => {
+          console.log(response);
+          alert('User created sucessfully');
+          // Navigate to the login page
+          this.router.navigate(['/login']); // Navigate to the dashboard page
+        },
+        (error) => {
+          console.log(error);
+          // TODO: Handle error
+          this.showError = true;
+        }
+      );
+  }
 }
